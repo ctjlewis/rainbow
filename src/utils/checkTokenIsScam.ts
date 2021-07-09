e9e14c78c5302a4386587f8be8f813f921d50692
C. Lewis <ctj.lewis@icloud.com>
2021-07-08 21:51:16 -0500

    feat: remote token list via `loadTokenList()` and `fromDiskCache()`

--- src/utils/checkTokenIsScam.ts
+++ src/utils/checkTokenIsScam.ts
@@ -1,7 +1,10 @@
 import { toLower } from 'lodash';
-import { OFFLINE_TOKEN_LIST_SAFE } from '@rainbow-me/references';
+import { loadTokenList } from 'rainbow-token-list';
 import { fromDiskCache } from 'rn-disk-cache';
 import { logger } from '.';
+import { RainbowToken } from '@rainbow-me/entities';
+import { getSafe } from '@rainbow-me/references';
+// import { OFFLINE_TOKEN_LIST_SAFE } from '@rainbow-me/references';
 
 interface TokenMetadata {
   name: string;
@@ -15,19 +18,15 @@ export default async function checkTokenIsScam({
   name,
   symbol,
 }: TokenMetadata): Promise<boolean> {
-  // logger.log('loadTokenList');
-  // logger.log({ loadTokenList });
-  // const tokenList = await fromDiskCache(
-  //   'tokenList',
-  //   async () => {
-  //     const liveTokenList: RainbowToken[] = await loadTokenList(fetch);
-  //     return getSafe(liveTokenList);
-  //   },
-  //   60 // one minute
-  // );
-  const test = await fromDiskCache('test', async () => ({ test: 42 }), 60);
-  logger.log(test);
-  const tokenList: any = OFFLINE_TOKEN_LIST_SAFE;
+  const tokenList = await fromDiskCache(
+    'tokenList',
+    async () => {
+      const liveTokenList: RainbowToken[] = await loadTokenList();
+      return getSafe(liveTokenList);
+    },
+    60 // one minute
+  );
+
   const nameFound = tokenList[toLower(name)];
   const symbolFound = tokenList[toLower(symbol)];
   return !!nameFound || !!symbolFound;
